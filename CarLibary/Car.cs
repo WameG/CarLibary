df92c01aedff7618cbf8abf2ed0919ef3da8588f
WameG <wgassama@yahoo.dk>
2023-03-05 20:20:25 +0100

    Add project files.

---
+++ CarLibary/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibary
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public string LicensePlate { get; set; }
+
+        public void ValidateID()
+        {
+            if (Id < 0) throw new ArgumentOutOfRangeException();
+        }
+
+        public void ValidateModel()
+        {
+            if (Model == null) throw new ArgumentNullException();
+
+            if (Model.Length < 4) throw new ArgumentOutOfRangeException();
+        }
+
+        public void ValidatePrice()
+        {
+            if (Price < 0) throw new ArgumentOutOfRangeException();
+        }
+
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate == null) throw new ArgumentNullException();
+
+            if (LicensePlate.Length < 2 || LicensePlate.Length > 7) throw new ArgumentOutOfRangeException();
+        }
+
+        public void Validate()
+        {
+            ValidateID();
+            ValidateModel();
+            ValidatePrice();
+            ValidateLicensePlate();
+        }
+    }
+}
---
+++ CarLibaryTests/CarTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarLibary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car car = new Car() { Id = 1, Model = "C220D", LicensePlate = "JKJ42", Price = 100000 };
+        Car carNegativeId = new Car() { Id = -1, Model = "C220D", LicensePlate = "JKJ42", Price = 100000 };
+        Car carModelNull = new Car() { Id = 1, Model = null, LicensePlate = "JKJ42", Price = 100000 };
+        Car carModelLessThan4 = new Car() { Id = 1, Model = "C22", LicensePlate = "JKJ42", Price = 100000 };
+        Car carNegativePrice = new Car() { Id = 1, Model = "C22", LicensePlate = "JKJ42", Price = -29673 };
+        Car carLicensePlateNull = new Car() { Id = 1, Model = "C22", LicensePlate = null, Price = -29673 };
+        Car carLicensePlateCharLessThan2 = new Car() { Id = 1, Model = "C22", LicensePlate = "I", Price = -29673 };
+        Car carLicensePlateCharGreaterThan7 = new Car() { Id = 1, Model = "C22", LicensePlate = "IOFJD78KD", Price = -29673 };
+
+        [TestMethod()]
+        public void ValidateIDTest()
+        {
+            car.ValidateID();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carNegativeId.ValidateID());
+        }
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentNullException>(() => carModelNull.ValidateModel());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carModelLessThan4.ValidateModel());
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carNegativePrice.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            car.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentNullException>(() => carLicensePlateNull.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carLicensePlateCharLessThan2.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carLicensePlateCharGreaterThan7.ValidateLicensePlate());
+        }
+    }
+}
